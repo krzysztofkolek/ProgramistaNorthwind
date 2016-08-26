0b488424f2590d65e131501dd709df5c9c6aa77f
krzysztof.kolek.praca@gmail.com <krzysztof.kolek.praca@gmail.com>
2016-08-26 09:57:32 +0200

    Welcome to Aurelia

--- Projekt/src/ProgramistaNorthwind/Startup.cs
+++ Projekt/src/ProgramistaNorthwind/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -51,6 +47,13 @@ namespace ProgramistaNorthwind
             app.UseApplicationInsightsExceptionTelemetry();
 
             app.UseMvc();
+
+            app.UseStaticFiles();
+            app.UseFileServer(new FileServerOptions
+            {
+                EnableDefaultFiles = true,
+                EnableDirectoryBrowsing = true,
+            });
         }
     }
 }
