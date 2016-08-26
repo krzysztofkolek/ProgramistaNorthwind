d3a02bec3560311ee2a31bd8e3339ea7cf9bf1ac
krzysztof.kolek.praca@gmail.com <krzysztof.kolek.praca@gmail.com>
2016-08-26 10:25:36 +0200

    Bootstrap  + Dodanie Bootstrapa  + Utworzenie prostego menu

--- Projekt/src/ProgramistaNorthwind/wwwroot/app.js
+++ Projekt/src/ProgramistaNorthwind/wwwroot/app.js
@@ -2,4 +2,12 @@ export class App {
     constructor() {
         this.message = "Welcome to Aurelia!";
     }
+
+    configureRouter(config, router) {
+        this.router = router;
+
+        config.map([
+			{ route:["", "home"], moduleId:"src/Views/home", title:"Home", nav:true, name:"home"}
+        ]);
+    }
 }
